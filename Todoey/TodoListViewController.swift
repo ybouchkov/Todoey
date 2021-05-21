76db0dd464021be0debce6c5893e7c0ed4d46ea2
Yani Buchkov <ybuchkov@tfxcorp.com>
2021-05-21 10:49:39 +0300

    🎨 234. Subclassing a UITableViewController

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    // MARK: - IBOutlets & Properties
+    private let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    private let reuseIdentifier = "ToDoItemCell"
+    private let rowHeight: CGFloat = 50.0
+    
+    // MARK: - ViewController Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Private:
+    
+}
+
+// MARK: - TableView Methods
+extension TodoListViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return rowHeight
+    }
+}
